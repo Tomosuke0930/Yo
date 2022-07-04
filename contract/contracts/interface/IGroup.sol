b157b8ac0c596643816c4b8d8747d24ed18fdd59
Tomosuke0930 <tomotomo09303396@gmail.com>
2022-07-04 14:08:48 +0900

    add: groups

--- contract/contracts/interface/IGroup.sol
+++ contract/contracts/interface/IGroup.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.4;
 
 interface IGroup  {
     struct Group {
-        bytes32 id;
-        bytes32 nullfier;
-        bytes32 name;
-        uint32 criteria;
-        bytes32 attr_key;
+        uint256 id;
+        uint256 nullfier;
+        string name;
+        uint256 criteria;
+        string attr_key;
     }
 }
