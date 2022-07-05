f60c33ad4b81e3c42c4d8756f4b78aff7c5531da
Tomosuke0930 <tomotomo09303396@gmail.com>
2022-07-05 15:48:51 +0900

    fix: other-files

--- contract/contracts/interface/ICommitment.sol
+++ contract/contracts/interface/ICommitment.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.4;
 
 interface ICommitment  {
+
+    /// type of commitment
     struct Commitment {
         uint256 id;
-        string userId;
+        string userId;/// userId: `User#${address}`必要なさそうだったら消す
         uint256 groupId;
         bytes32 data;
         string metadata;
         address mintAddress;
-        uint256 createdAt;
+        uint256 createdAt; /// 必要なさそうだったら消す
     }
 }
