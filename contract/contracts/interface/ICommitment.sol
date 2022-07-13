7eef8e232486c42bf16b18300a9fc0783a15588a
Tomosuke0930 <tomotomo09303396@gmail.com>
2022-07-13 15:06:43 +0900

    add: natspec-of-ICommitment

--- contract/contracts/interface/ICommitment.sol
+++ contract/contracts/interface/ICommitment.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-interface ICommitment {
-    /// type of commitment
+/// @title Commitment interface for Yo V1
+/// @notice The type of Commitment in Yo V1
+interface ICommitment  {
+
+    /// @notice - Commitment is used to prove the member of groups in Semaphore library: http://semaphore.appliedzkp.org/
+    /// @dev - Semaphore allows Ethereum users to prove their membership of a group using Zero Knowledge Proof
+    /// @param id - The identifier of commitment 
+    /// @param userId - The identifier of user
+    /// @param groupId - The idnetifier of group, each group has own Merkle Tree
+    /// @param data - The hash of commitment
+    /// @param metadata - The metadata is used to mint NFT
+    /// @param mintAddress - The destination address to send NFT
+    /// @param createdAt - The time creating this commitment
     struct Commitment {
         uint256 id;
-        string userId; /// userId: `User#${address}`必要なさそうだったら消す
+        string userId;
         uint256 groupId;
-        bytes32 data;
+        bytes32 hash;
         string metadata;
         address mintAddress;
-        uint256 createdAt; /// 必要なさそうだったら消す
+        uint256 createdAt;
     }
 }
