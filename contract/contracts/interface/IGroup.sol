3c357d22eb98c0428b636b7591b0288560c07a5a
Tomosuke0930 <tomotomo09303396@gmail.com>
2022-07-13 15:07:14 +0900

    add: natspec-of-IGroup.sol

--- contract/contracts/interface/IGroup.sol
+++ contract/contracts/interface/IGroup.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-interface IGroup {
+/// @title Group interface for Yo V1
+/// @notice The type of Group in Yo V1
+interface IGroup  {
+
+    /// @param id - The identifier of group 
+    //@audit check
+    /// @param nullifier - The private values of the Semaphore identity.[WIP]
+    /// @param name - The name of this group
+    /// @param criteria - This value is in group.json file. This value show us the proficiency of your skill
+    /// @param attr_key - This value is in group.json file. This value used to identify for NGT
     struct Group {
         uint256 id;
-        uint256 nullfier;
+        uint256 nullifier;
         string name;
         uint256 criteria;
         string attr_key;
     }
 }
-/**
-    group.example
-  {
-    "name": "100+ public repos",
-    "criteria": 100,
-    "attr_key": "public_repos"
-  }
- */
