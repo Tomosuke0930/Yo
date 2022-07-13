f7535383964c184f7f70df9b3112f5850daa8c67
Tomosuke0930 <tomotomo09303396@gmail.com>
2022-07-13 16:58:01 +0900

    update: Commitment & natspcec

--- contract/contracts/Commitment.sol
+++ contract/contracts/Commitment.sol
@@ -5,67 +5,65 @@ import './interface/ICommitment.sol';
 
 contract Commitment is ICommitment {
 
-    mapping(address => Commitment[]) userCommitments;
+    mapping(address => mapping(uint256 =>Commitment)) userCommitments;
+    uint256 constant MAX_COMMITMENT_LENGTH = 9;
     
     /************************************************
      *  Getters
      ***********************************************/
 
-    // commitment is so long
-    // commitments => coms
-
-    /// get user's commitment length
-    function getComsLength(address _address) public view returns (uint256 length) {
-        length = userCommitments[_address].length;
-    }
-
-    /// get user's all commitments
-    function getAllComs(address _address) public view returns (Commitment[] memory commitments) {
-        uint256 length = getComsLength(_address);
-        for (uint256 i = 0; i < length; i++) {
-            commitments[i] = userCommitments[_address][i];
-        }
-    }
-
-    /// get target commitment
-    function getTargetCom(address _address, uint256 _id) private view returns (Commitment memory commitment) {
-        uint256 length = getComsLength(_address);
-        Commitment[] memory commtiments = getAllComs(_address);
-        uint256 targetIndex;
-        for (uint256 i = 0; i < length; i++) {
-            if (commtiments[i].id == _id) targetIndex = i;
-        }
-        commitment = userCommitments[_address][targetIndex];
+    /**
+     * @notice Gets the last day of the month
+     * @param _address is user address
+     * @param _id is commitment identfier
+     * @return commitment from the userCommitment mapping 
+     */
+    function getTargetCommitment(address _address, uint256 _id) external view returns (Commitment memory commitment) {
+        commitment = userCommitments[_address][_id];
     }
 
     /************************************************
      *  Add
      ***********************************************/
 
-    /// add commitment
-    function addCom(
+    /**
+     * @notice Add commitment
+     * @param _address is user address
+     * @param _id is commitment identfier
+     * @param _hash is commitment hash
+     * @param _groupId is identifier of group from group.json
+     * @param _userId is user identifier
+     * @param _createdAt is the time when creating this commitment 
+     */
+    function addCommitment(
         address _address,
         uint256 _id,
-        bytes32 _data,
+        bytes32 _hash,
         uint256 _groupId,
-        string calldata _userId,
+        bytes32 _userId,
         uint256 _createdAt
     ) external {
-        userCommitments[_address].push(Commitment(_id, _userId, _groupId, _data, '', address(0), _createdAt));
+        userCommitments[_address][_id] = Commitment(_id, _userId, _groupId, _hash,address(0), _createdAt,"");
     }
 
     /************************************************
      *  Update
      ***********************************************/
 
-    /// update commitment
-    function updateCom(
+    /**
+     * @notice Update commitment
+     * @param _metadta is metadata of NFT
+     * @param _address is user address to send NFT
+     * @param _address is user address
+     * @param _id is commitment identfier
+     */
+    function updateCommitment(
         string calldata _metadta,
         address _mintAddress,
         address _address,
         uint256 _id
-    ) external view {
-        Commitment memory commitment = getTargetCom(_address, _id);
+    ) external {
+        Commitment storage commitment = userCommitments[_address][_id];
         commitment.metadata = _metadta;
         commitment.mintAddress = _mintAddress;
     }
